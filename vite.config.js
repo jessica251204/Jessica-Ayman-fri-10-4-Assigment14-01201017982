06213de48fb4df6488c02cbfb4ce6cfcc0d01a8c
Jessica Ayman <jessy.ayman.ja@gmail.com>
2026-09-17 15:27:20 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
-  base:'/jessica251204/Jessica-Ayman-fri-10-4-Assigment14-01201017982/'
+  base:'Jessica-Ayman-fri-10-4-Assigment14-01201017982/'
 })
