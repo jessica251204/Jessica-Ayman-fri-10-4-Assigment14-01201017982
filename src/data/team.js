8c0a393e822a6c190718fa9aa7e72423b8d52fb6
Jessica Ayman <jessy.ayman.ja@gmail.com>
2026-09-17 14:20:53 +0300

    commit

--- src/data/team.js
+++ src/data/team.js
@@ -1,9 +1,5 @@
-// بيستخرج أعضاء الفريق تلقائيًا من بيانات الـ author جوه كل بوست
-// عشان منكررش نفس البيانات في مكانين. غيّر مسار الاستيراد لو اسم ملف البوستات مختلف عندك.
 import posts from "./posts";
-
 const uniqueAuthors = new Map();
-
 posts.forEach((post) => {
   if (!uniqueAuthors.has(post.author.name)) {
     uniqueAuthors.set(post.author.name, {
